85c94625bb69878ebabed15f9e95ef677c913350
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2011-08-25 15:40:28 +0530

    create the scaffold for the application code-base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoolRide::Application.config.secret_token = 'a8ca06fa331bfe7e9fb63357952dd929a977c51976af93b45d57f04de77c5f0271d0af7798657c8f263ee7b4a27c44711805aeef218147082fd879417e92a788'
