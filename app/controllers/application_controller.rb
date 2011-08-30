73e455441cba9da8cf009ad3d1d421ad8dd94003
Pooja Gupta <pooja.g@imaginea.com>
2011-08-30 14:08:45 +0530

    Integration of auto search for locations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!,:except => [:auto_search]
+  
+  
+  def auto_search
+    @result = (APP_LOCATIONS.map do |l|
+        l if l.to_s.match(params[:q].to_s.titleize)
+      end).compact
+    respond_to do |wants|
+      wants.json {
+        render :json => @result
+      }
+    end
+  end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  describe "#Auto Search" do
+    it "should return matched locations" do
+      get 'auto_search',{:q => 'madh'}  
+      assigns[:result].should_not be nil
+      assigns[:result].class.should == Array
+      assigns[:result].size.should == 1
+      assigns[:result].first.should == "Madhapur"
+    end
+    it "should return empty array of locations " do
+      get 'auto_search',{:q => 'xxxx'},:format => :json
+      assigns[:result].class.should == Array
+      assigns[:result].first.should == nil
+    end
+  end
+end
