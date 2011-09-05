5bd65623a340fe044eda0207b33f2510075b6c58
Pooja Gupta <pooja.g@imaginea.com>
2011-09-05 12:19:14 +0530

    Fixed Bug in Auto search,Rewrite the search value before search.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   def auto_search
     @result = (APP_LOCATIONS.map do |l|
-        l if l.to_s.match(params[:q].to_s.titleize)
+        l if l.to_s.match(params[:q].to_s.try(:downcase).try(:titleize))
       end).compact
     respond_to do |wants|
       wants.json {
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -14,5 +14,12 @@ describe ApplicationController do
       assigns[:result].class.should == Array
       assigns[:result].first.should == nil
     end
+    it "should rewrite the search value before search " do
+      get 'auto_search', {:q => 'MadHap'},:format => :json
+      assigns[:result].should_not be nil
+      assigns[:result].class.should == Array
+      assigns[:result].size.should == 1
+      assigns[:result].first.should == "Madhapur"
+    end
   end
 end
