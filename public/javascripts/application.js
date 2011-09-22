6537314711fcbbeb05c592590e73e4b316825eae
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2011-09-22 13:51:07 +0530

    make changes to the landing page search, about page etc

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,43 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
+  initCBSwitch();
   AutoSearchLocation();
+  initRideSearch();
   $("a[rel]").overlay({
     mask: 'darkred',
     effect: 'apple'
   });
 });
+
+var initCBSwitch = function() {
+	$('#search_get_ride').hide();
+	$('#search_offer_ride').hide();
+	$(".cb-enable").click(function(){
+		var parent = $(this).parents('.switch');
+		$('.cb-disable',parent).removeClass('selected');
+		$(this).addClass('selected');
+		$('.checkbox',parent).attr('checked', true);
+	});
+	$(".cb-disable").click(function(){
+		var parent = $(this).parents('.switch');
+		$('.cb-enable',parent).removeClass('selected');
+		$(this).addClass('selected');
+		$('.checkbox',parent).attr('checked', false);
+	});	
+}
+
+var initRideSearch = function() {
+  var search_url = $('#search_rides').attr('search_url');
+  $('#search_rides').click(function() {
+	 var origin = $('#search_origin').val();
+	 var destination = $('#search_destination').val();	 
+	 if (origin == '' || destination == '') {
+		$('#search_error').fadeIn(400);
+	 }
+	 else { $('#search_error').hide(); }
+	});	
+}
 
 var AutoSearchLocation = function(){
   var url = $('#data-url').val();
