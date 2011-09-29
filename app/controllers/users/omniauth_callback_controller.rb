92c6f3ab2160c5b77e018b7ac808d019b637f836
Amar Singh Kaithwar <amar.k@imaginea.com>
2011-09-29 15:53:59 +0530

    Removed flash messages for registrations from Facebook.

--- app/controllers/users/omniauth_callback_controller.rb
+++ app/controllers/users/omniauth_callback_controller.rb
@@ -3,9 +3,9 @@ class Users::OmniauthCallbackController < ApplicationController
     # You need to implement the method below in your model
     @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
     if @user && @user.persisted?
-      if !user_signed_in?
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      end
+#      if !user_signed_in?
+#        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+#      end
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.facebook_data"] = env["omniauth.auth"]
