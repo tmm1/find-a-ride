50a9f5e56550210427e1063451dd0cc646025caa
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2011-10-12 10:50:49 +0530

    add mini dp to the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_display_pic(user, size)
+    if user.photo_file_name
+      image_tag(user.photo.url, :size => size)
+    else
+      image_tag('blank_profile_picture.png', :size => size)
+    end
+  end
 end
