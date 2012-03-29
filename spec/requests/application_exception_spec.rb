dd665fb6d4ce8bdee88b7a2bf751ac6062906f59
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2012-03-29 14:47:25 +0530

    rename the file and change the spec description

--- spec/requests/application_exception_spec.rb
+++ spec/requests/application_exception_spec.rb
@@ -1,20 +1,18 @@
 require 'spec_helper'
 
-describe "Application management" do
- 
-  it "accessing unknown action and render not found page" do
-    get "somthing"
+describe "application exceptions" do
+  it "should render not found page on routing to an unknown action" do
+    get "unknown"
     response.status.should eql 404
     response.should render_template(:not_found)
   end
 
-  it "accessing search with out params and render internal server error page" do
+  it "should render internal server error page on server error" do
     @login_user = Factory(:user)
     @login_user.confirm!
-    post "/users/sign_in" , :user =>{:email => @login_user.email , :password => "test1234" }  
+    post "/users/sign_in", :user =>{:email => @login_user.email , :password => "test1234" }
     get "ride_requests/search" 
     response.status.should eql 500
     response.should render_template(:internal_server_error)
   end
-
 end
