b52f0212ae4f142496eb0dc754b45dd7e42c563a
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2012-04-04 16:56:03 +0530

    refactor to make it look little more elegant

--- lib/validators/date_time_validator.rb
+++ lib/validators/date_time_validator.rb
@@ -1,8 +1,8 @@
 class DateTimeValidator < ActiveModel::Validator
   def validate(record)
-    if (record.start_date && !record.start_date.blank?)
-      if (DateTime.parse(record.start_date).strftime("%d-%m-%Y") == Time.now.strftime("%d-%m-%Y"))
-        if (!record.start_time.blank? && Time.parse(record.start_time) <= Time.now)
+    unless record.start_date.blank? || record.start_time.blank?
+      if DateTime.parse(record.start_date).strftime("%d-%m-%Y") == Time.now.strftime("%d-%m-%Y")
+        if Time.parse(record.start_time) <= Time.now
           record.errors[:start_time] << "can't be in the past"
         end
       end
