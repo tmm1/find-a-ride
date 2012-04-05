9c3c1a8b8d6a8a0e7ea43d76f95a383c06c68de9
Anil kumar <anil.r@imaginea.com>
2012-04-05 11:53:13 +0530

    Removed conflicted code from hook_up factory.

--- spec/factories/hook_up.rb
+++ spec/factories/hook_up.rb
@@ -3,6 +3,4 @@ Factory.define :hook_up do |h|
   h.association :contactee, :factory => :user
   h.association :hookable, :factory => :ride_request
   h.message {Faker::Lorem.paragraph}
-  h.hookable_type 'RideRequest'
-  h.association :hookable_id, :factory => :ride
 end
