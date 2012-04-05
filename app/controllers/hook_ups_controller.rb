8030523d2b4a50a1574a47ec7e6acb70b668a409
Anil kumar <anil.r@imaginea.com>
2012-04-05 15:39:15 +0530

    Some changes related to hookup code and added spec for hook_up_helper.

--- app/controllers/hook_ups_controller.rb
+++ app/controllers/hook_ups_controller.rb
@@ -12,14 +12,7 @@ class HookUpsController < ApplicationController
 
   def create
     @hook_up = HookUp.new(params[:hook_up])
-    mobile = params[:hook_up][:mobile]
-    type = params[:hook_up][:ride_type]
     if @hook_up.save
-      if type == "ride_request"
-        HookupMailer.ride_offerer_email(@hook_up,mobile).deliver
-       else
-        HookupMailer.ride_requestor_email(@hook_up,mobile).deliver
-      end
       render :text => 'success', :status => 200
     else
       render :text => 'failed', :status => 200
