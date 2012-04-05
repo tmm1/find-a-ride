2e9ffdc2f83169fa4d5b6c7d9b1696581cde7f4b
Amar Singh Kaithwar <amar.k@imaginea>
2012-04-05 14:42:35 +0530

    Added custom validation for the duplicate record and wrote some test cases.

--- lib/validators/duplicate_ride_validator.rb
+++ lib/validators/duplicate_ride_validator.rb
@@ -4,7 +4,7 @@ class DuplicateRideValidator < ActiveModel::Validator
     dest = Location.find_by_name(record.dest)
     ride_time = Helper.to_datetime(record.start_date, record.start_time)
     return if (orig.blank? || dest.blank? || ride_time.blank?)
-    dup = Ride.find_by_origin_and_destination_and_ride_time_and_user_id(orig, dest, ride_time, record.user_id)
+    dup = Ride.find_by_origin_and_destination_and_ride_time_and_user_id_and_type(orig, dest, ride_time, record.user_id, record.type)
     record.errors[:base] << "Oops. You already put in one with similar criteria!"  unless dup.blank?
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
 
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -16,10 +17,12 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.include(CustomMatchers)
+
   config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-#  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
