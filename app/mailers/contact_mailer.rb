b613f3dd6f4801a2e7ebe18b1749b78cd4913512
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2012-04-10 11:34:36 +0530

    rebrand the app *OnTheWay*, add new logo

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default :from => "no-reply@find-a-ride.com"
+  default :from => "no-reply@ontheway.com"
   layout 'mailer'
   
   def contact_email(info)
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
                 success: function(data) {
                     $('#contact').modal('hide');
                     if (data === 'success') {
-                        $('.notice-area').html("<div class='alert alert-success'>Your message has been successfully sent.</div>")
+                        $('.notice-area').html("<div class='alert alert-success'>Thanks for your input!</div>")
                     }
                     else {
                         $('.notice-area').html("<div class='alert alert-error'>There was a problem. Please retry later.</div>")
@@ -279,125 +279,5 @@ function setAppCity(location){
 }
 
 
-// ********* OLD CODE ********* DONT DELETE FOR NOW ***//
-// 
-// var constructContactMsg = function(origin, dest, matcher) {
-//     if (matcher == 'drivers') {
-//         return 'I see that you are looking to offer a ride from '+origin+' to '+dest+'. I m interested. Please get in touch with me so we can make this happen.'
-//     }
-//     else if (matcher == 'riders') {
-//         return 'I see that you are looking to find a ride from '+origin+' to '+dest+'. I would be willing to share a ride with you. Please get in touch with me so we can make this happen.'
-//     }
-// }
 
-
-// var initContactForm = function() {
-//     $('.form_wrapper').find('.submit_button').click(function(e) {
-//         e.preventDefault();
-//         form_div = $(this).closest('.form_wrapper');
-//         var contact_url = $(this).attr('post_url');
-//         var contactor_id = form_div.find('#contacter_id').val();
-//         var msg = form_div.find('#contact_msg').val();
-//         var contactee_id = form_div.find('#contactee_id').val();
-//         var origin = $('#origin').val();
-//         var dest = $('#dest').val();
-//         var matcher = $('#matcher').val();
-//         if (typeof contactor_id == 'undefined') {
-//             var name = form_div.find('#contact_name').val();
-//             var email = form_div.find('#contact_email').val();
-//             var phone = form_div.find('#contact_phone').val();
-// 
-//             if (name == '' || email == '') {
-//                 form_div.find('.form_error').html('<h3>Need both name and email!</h3>');
-//                 form_div.find('.form_error').fadeIn(400);
-//             }
-//             else if (!isValidEmail(email)) {
-//                 form_div.find('.form_error').html('<h3>Need a valid email!</h3>');
-//                 form_div.find('.form_error').fadeIn(400);
-//             }
-//             else {
-//                 postContactMsg(contact_url, {
-//                     contactee_id: contactee_id,
-//                     matcher: matcher,
-//                     user_info: {
-//                         name: name,
-//                         email: email,
-//                         phone: phone
-//                     },
-//                     message: msg
-//                 });
-//             }
-//         }
-//         else {
-//             postContactMsg(contact_url, {
-//                 contactor_id: contactor_id,
-//                 contactee_id: contactee_id,
-//                 matcher: matcher,
-//                 message: msg
-//             });
-//         }
-//     });
-// }
-// 
-// var postContactMsg = function(contact_url, post_data) {
-//     $.ajax({
-//         type: 'post',
-//         url: contact_url,
-//         data: post_data,
-//         success: function(data) {
-//             $('.form_error').hide();
-//             $('.contact_form').hide();
-//             $('.contact_response').html(data);
-//             $('.contact_response').fadeIn(400);
-//         },
-//         failure: function(data) {
-//         }
-//     });
-// }
-// 
-// var resetContactForm = function() {
-//     var origin = $('#origin').val();
-//     var dest = $('#dest').val();
-//     var matcher = $('#matcher').val();
-//     $('.contact_response').hide();
-//     $('.form_wrapper').each(function (){
-//         $(this).find('.form_error').hide();
-//         $(this).find('#contact_name').val('');
-//         $(this).find('#contact_email').val('');
-//         $(this).find('#contact_phone').val('');
-//         $(this).find('#contact_msg').val(constructContactMsg(origin, dest, matcher));
-//     });
-//     $('.contact_form').show();
-// }
-// 
-// var locationSearch = function(){
-//     var url = $('#data-url').val();   
-//     var elem = $("input.autocomplete");
-//     elem.unautocomplete();
-//     elem.autocomplete(url,{
-//         dataType: 'json',
-//         extraParams: {
-//             city: $("#city_select").val()
-//         },
-//         delay :200,
-//         scroll: true,
-//         scrollHeight: 300,
-//         parse: function(data) {         
-//             data = jQuery.grep(data, function (a) { return a != $("#ride_request_orig").val(); });            
-//             var array = new Array();
-//             for(var i=0; i<data.length; i++)
-//             {
-//                 array[array.length] = {
-//                     data: data[i],
-//                     value: data[i],
-//                     result:data[i]
-//                 };
-//             }
-//             return array;
-//         },
-//         formatItem: function(row) {
-//             return row;
-//         }
-//     });
-// }
 
