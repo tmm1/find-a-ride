531789f106727d5915f5a2fcfcf572163166eea5
sivakrishna <sivakrishna.m@imaginea.com>
2012-04-19 11:20:44 +0530

    #28217205  sharing my landline number from my profile (if updated) in the offer/request hook up email notification

--- spec/mailers/hookup_mailer_spec.rb
+++ spec/mailers/hookup_mailer_spec.rb
@@ -22,9 +22,31 @@ describe HookupMailer do
       email = HookupMailer.ride_requestor_email(@hook_up,@hook_up.mobile).deliver
       email.subject.should == 'Message from OnTheWay user'
       email.to.should == [@hook_up.contactee.email]
+      puts "==============================="
+      puts email.body
       email.body.include?("#{@hook_up[:message]}").should be_true
       email.body.include?("#{@hook_up[:mobile]}").should be_true
+      email.body.raw_source.should_not include "landline:"
     end
+
+    it 'should deliver the email with out the landline info' do
+      @hook_up.mobile = '9999988888'
+      email = HookupMailer.ride_requestor_email(@hook_up,@hook_up.mobile).deliver
+      email.subject.should == 'Message from OnTheWay user'
+      email.to.should == [@hook_up.contactee.email]  
+      email.body.raw_source.should_not include "landline:"
+    end
+    
+    it 'should deliver the email with the landline info if user profile updates landline' do
+      @hook_up.mobile = '9999988888'
+      @hook_up.contacter.update_attributes({:landline => "0403535353"})
+      email = HookupMailer.ride_requestor_email(@hook_up,@hook_up.mobile).deliver
+      email.subject.should == 'Message from OnTheWay user'
+      email.to.should == [@hook_up.contactee.email]  
+      email.body.raw_source.should include "landline:"
+    end
+
+
   end
   
   describe '#ride offerer hook-up email' do
@@ -42,6 +64,22 @@ describe HookupMailer do
       email.subject.should == 'Message from OnTheWay user'
       email.to.should == [@hook_up.contactee.email]
       email.body.include?("#{@hook_up[:message]}").should be_true
+    end
+
+
+    it 'should deliver the email with out the landline info' do
+      email = HookupMailer.ride_offerer_email(@hook_up,@hook_up.mobile).deliver
+      email.subject.should == 'Message from OnTheWay user'
+      email.to.should == [@hook_up.contactee.email]
+      email.body.raw_source.should_not include "landline:"
+    end
+
+    it 'should deliver the email with the landline info if user profile updates landline' do
+      @hook_up.contacter.update_attributes({:landline => "0403535353"})
+      email = HookupMailer.ride_offerer_email(@hook_up,@hook_up.mobile).deliver
+      email.subject.should == 'Message from OnTheWay user'
+      email.to.should == [@hook_up.contactee.email]
+      email.body.raw_source.should include "landline:"
     end
   end
     
