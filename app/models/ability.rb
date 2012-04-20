fcb2b1dba8b91edb1db0e0385be3be72a631b84a
Karthik Muthupalaniappan <karthik.swaminathan@gmail.com>
2012-04-20 11:32:58 +0530

    break down the ability rules

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,13 @@ class Ability
   def initialize(user)
     if user #logged in user access
       can :read, [Ride, RideOffer, RideRequest, HookUp]
-      can :manage, [Ride, RideOffer, RideRequest, HookUp], :user_id => user.id
+      can :manage, [Ride, RideOffer, RideRequest], :user_id => user.id
+      can :manage, HookUp, :contacter_id => user.id
+      can :read, Alert, :receiver_id => user.id
+      can :manage, Alert, :sender_id => user.id 
     end
     
     # anonymous user access
     can :read, [City, Location]
   end
-  
 end
