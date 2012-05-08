99161c62e59e2bd48c26a6640fdb2cde346fe44a
Vikas Reddy <vikas.r@imaginea.com>
2012-05-08 10:47:15 +0530

    #29069207: Modified uri matchers for showing 'active' sidebar entry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,16 +7,32 @@ module ApplicationHelper
     end
   end
 
+  def uri_matches?(paths)
+    paths ||= []
+    regex = %r|^([^?]*)(\??.*)$|
+    paths.any? do |path|
+      request.fullpath.sub(regex, '\1') == path.sub(regex, '\1')
+    end
+  end
+
   def sidebar_entries
+    entries = [
+      {:text => "Search",         :paths => [search_rides_path, search_ride_requests_path, search_ride_offers_path]},
+      {:text => "Post an offer",  :paths => [new_ride_offer_path, ride_offers_path]},
+      {:text => "Request a ride", :paths => [new_ride_request_path, ride_requests_path]},
+      {}, # divider
+      {:text => "My requests and offers", :paths => [list_user_rides_path(current_user)]},
+      {:text => "My recent activity",     :paths => [recent_path(current_user)]}
+    ]
+
     content = ''
-    [ {:text => "Search", :href => search_rides_path},
-      {:text => "Post an offer", :href => new_ride_offer_path},
-      {:text => "Request a ride", :href => new_ride_request_path},
-      {:text => "My requests and offers", :href => list_user_rides_path(current_user)},
-      {:text => "My recent activity", :href => recent_path(current_user)}
-    ].each do |entry|
-      content << content_tag(:li, :class => (request.fullpath == entry[:href] ? 'active' : '')) do
-        link_to(entry[:text], entry[:href])
+    entries.each do |entry|
+      if entry.empty?
+        content << content_tag(:li, nil, :class => 'divider')
+      else
+        content << content_tag(:li, :class => uri_matches?(entry[:paths]) ? 'active' : '') do
+          link_to entry[:text], entry[:paths].first
+        end
       end
     end
     content.html_safe
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -16,6 +16,30 @@ describe ApplicationHelper do
     tag.should == image_tag('blank_profile_picture.jpg', :alt => 'Pic', :size => '100x100')
   end
 
+  describe '#uri_matches?' do
+    before(:each) do
+      helper.request.stub!(:fullpath).and_return(search_ride_offers_path)
+    end
+
+    it 'should return true if the uri matches any of the paths given' do
+      helper.uri_matches?([search_rides_path, search_ride_requests_path, search_ride_offers_path]).should be_true
+      helper.uri_matches?([search_ride_offers_path]).should be_true
+    end
+
+    it 'should match the uri irrespective of arguments' do
+      helper.uri_matches?([search_ride_offers_path(:dest => 'Madhapur', :orig => 'Kukatpally')]).should be_true
+    end
+
+    it 'should return false only if the uri doesn\'t match all of the paths given' do
+      helper.uri_matches?([search_ride_requests_path, search_rides_path]).should be_false
+    end
+
+    it 'should return false and not raise error if an empty \'paths\' is given' do
+      helper.uri_matches?([]).should be_false
+      helper.uri_matches?(nil).should be_false
+    end
+  end
+
   describe '#sidebar_entries' do
     before(:each) do
       @user = Factory(:user)
@@ -24,10 +48,11 @@ describe ApplicationHelper do
     it 'should include the five entries' do
       sign_in @user
       tag = helper.sidebar_entries
-      tag.scan('<li').should have(5).things
+      tag.scan('<li').should have(6).things
       tag.should include(link_to("Search", search_rides_path))
       tag.should include(link_to("Post an offer", new_ride_offer_path))
       tag.should include(link_to("Request a ride", new_ride_request_path))
+      tag.should include(content_tag(:li, nil, :class => 'divider'))
       tag.should include(link_to("My requests and offers", list_user_rides_path(@user)))
       tag.should include(link_to("My recent activity", recent_path(@user)))
     end
