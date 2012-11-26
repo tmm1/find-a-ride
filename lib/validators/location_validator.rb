64dae8be9dba14c6aa18ade26483257efdbc7070
Aman Gupta <aman@tmm1.net>
2012-11-25 21:23:27 -0800

    fix nil city exceptions

--- lib/validators/location_validator.rb
+++ lib/validators/location_validator.rb
@@ -2,7 +2,7 @@ class LocationValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
     city = City.find_by_name(record.current_city)
-    unless city.locations.find_by_name(value.downcase.titleize)
+    unless city && city.locations.find_by_name(value.downcase.titleize)
       record.errors[attribute] << 'is not valid'
     end
   end
